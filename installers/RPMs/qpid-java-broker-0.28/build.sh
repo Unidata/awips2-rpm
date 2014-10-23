bfb8ac9cedf03e877734fda54e0a3744af98cd0e
David Lovely <David.N.Lovely@raytheon.com>
2014-10-23 16:03:48 -0500

    Omaha #2991 Build script updates for qpid 0.28.

--- installers/RPMs/qpid-java-broker-0.28/build.sh
+++ installers/RPMs/qpid-java-broker-0.28/build.sh
@@ -1,57 +1,23 @@
 #!/bin/bash
 
-if [ -z ${AWIPSII_BUILD_ROOT} ]; then
-   export AWIPSII_BUILD_ROOT="/tmp/${USER}/awips2-qpid-java-broker"
-   echo "INFO: using default build root - ${AWIPSII_BUILD_ROOT}."
-fi
-
 path_to_script=`readlink -f $0`
 dir=$(dirname $path_to_script)
 
 __SPECS=qpid-java-broker.spec
 
-export TOPDIR=${AWIPSII_BUILD_ROOT}
+mkdir -p ${AWIPSII_TOP_DIR}/RPMS
+mkdir -p ${AWIPSII_TOP_DIR}/SRPMS
+mkdir -p ${AWIPSII_TOP_DIR}/BUILD
+mkdir -p ${AWIPSII_TOP_DIR}/SOURCES
 
-# create the rpm directory structure
-if [ -d ${TOPDIR}/BUILD ]; then
-   rm -rf ${TOPDIR}/BUILD
-   if [ $? -ne 0 ]; then
-      exit 1
-   fi
-fi
-mkdir ${TOPDIR}/BUILD
+FOSSDIR=${WORKSPACE}/foss/qpid-java-broker-0.28/
 
-if [ -d ${TOPDIR}/SOURCES ]; then
-   rm -rf ${TOPDIR}/SOURCES
-   if [ $? -ne 0 ]; then
-      exit 1
-   fi
-fi
-mkdir ${TOPDIR}/SOURCES
+cp -v ${FOSSDIR}/packaged/qpid-java-broker-0.28.tar.gz ${AWIPSII_TOP_DIR}/SOURCES
 
-FOSSDIR=${dir}/../../../foss/qpid-java-broker/
-
-cp ${FOSSDIR}/packaged/qpid-java-broker-0.28.tar.gz ${TOPDIR}/SOURCES
-
-if [ -d ${TOPDIR}/RPMS ]; then
-   rm -rf ${TOPDIR}/RPMS
-   if [ $? -ne 0 ]; then
-      exit 1
-   fi
-fi
-mkdir ${TOPDIR}/RPMS
-
-if [ -d ${TOPDIR}/SRPMS ]; then
-   rm -rf ${TOPDIR}/SRPMS
-   if [ $? -ne 0 ]; then
-      exit 1
-   fi
-fi
-mkdir ${TOPDIR}/SRPMS
 
 # build the rpm
-rpmbuild -bb \
-   --define "_topdir ${TOPDIR}" \
+ rpmbuild -bb \
+   --define "_topdir ${AWIPSII_TOP_DIR}"/ \
    --define "_patchdir ${FOSSDIR}/src/patch" \
    ${__SPECS}
 if [ $? -ne 0 ]; then
